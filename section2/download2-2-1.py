778ff1e6d4f4ddad36d2b94f19c77affaaee5fae
hyeokjin-eun <gurwls2399@gmail.com>
2020-05-28 15:17:56 +0900

    download2-2-2 학습
    urlopen 학습 및 with문 작성

--- section2/download2-2-1.py
+++ section2/download2-2-1.py
@@ -3,8 +3,8 @@ import urllib.request as dw
 imgUrl = "http://post.phinf.naver.net/20160621_169/1466482468068lmSHj_JPEG/If7GeIbOPZuYwI-GI3xU7ENRrlfI.jpg"
 htmlUrl = "https://www.google.com/"
 
-savePath1 = "../save/test1.jpg"
-savePath2 = "../save/index.html"
+savePath1 = "../save/download2-2-1.jpg"
+savePath2 = "../save/download2-2-1.html"
 
 dw.urlretrieve(imgUrl, savePath1)
 dw.urlretrieve(htmlUrl, savePath2)
---
+++ section2/download2-2-2.py
@@ -0,0 +1,19 @@
+import urllib.request as dw
+
+imgUrl = "http://post.phinf.naver.net/20160621_169/1466482468068lmSHj_JPEG/If7GeIbOPZuYwI-GI3xU7ENRrlfI.jpg"
+htmlUrl = "https://www.google.com/"
+
+savePath1 = "../save/download2-2-2.jpg"
+savePath2 = "../save/downlaod2-2-2.html"
+
+file1 = dw.urlopen(imgUrl).read()
+file2 = dw.urlopen(htmlUrl).read()
+
+saveFile1 = open(savePath1, 'wb')
+saveFile1.write(file1)
+saveFile1.close()
+
+with open(savePath2, 'wb') as saveFile2 :
+    saveFile2.write(file2)
+
+print("다운로드 완료")
