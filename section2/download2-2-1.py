1846eb6536048d5953769031d401306bf91af344
hyeokjin-eun <gurwls2399@gmail.com>
2020-05-28 15:01:47 +0900

    download2-2-1 학습
    urlretrieve 를 활용한 파일 다운로드

---
+++ section2/download2-2-1.py
@@ -0,0 +1,12 @@
+import urllib.request as dw
+
+imgUrl = "http://post.phinf.naver.net/20160621_169/1466482468068lmSHj_JPEG/If7GeIbOPZuYwI-GI3xU7ENRrlfI.jpg"
+htmlUrl = "https://www.google.com/"
+
+savePath1 = "../save/test1.jpg"
+savePath2 = "../save/index.html"
+
+dw.urlretrieve(imgUrl, savePath1)
+dw.urlretrieve(htmlUrl, savePath2)
+
+print("다운로드 완료")
